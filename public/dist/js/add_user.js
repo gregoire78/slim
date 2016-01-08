50eac04d71e3a90276540bedbc163051174ddd2d
Grégoire <greg.joncour@gmail.com>
2016-01-08 23:13:45 +0100

    modif js

--- public/dist/js/add_user.js
+++ public/dist/js/add_user.js
@@ -3,6 +3,20 @@
  */
 $(document).ready(function($){
 
+    var champs = ['firstname', 'lastname', 'email', 'stree', 'city', 'postalcode', 'phone', 'group'];
+    var champs_json = {
+        'firstname' : $('#firstname'),
+        'lastname': $('#lastname'),
+        'email' : $('#email'),
+        'street' : $('#street'),
+        'city' : $('#city'),
+        'postalcode' : $('#postalCode'),
+        'phone' : $('#phone'),
+        'group' : $('#group')
+    };
+
+
+    //initiation variable FORM_GROUP_ID
     var form_firstname = 'form-firstname';
     var form_lastname = 'form-lastname';
     var form_email = 'form-email';
@@ -12,40 +26,45 @@ $(document).ready(function($){
     var form_phone = 'form-phone';
     var form_group = 'form-group';
 
-    var firstname = $('#firstname');
-    var lastname = $('#lastname');
-    var email = $('#email');
-    var street = $('#street');
-    var city = $('#city');
-    var postalcode = $('#postalCode');
-    var phone = $('#phone');
-    var group = $('#group');
+    //initiation variable selector INPUT
+    var firstname = champs_json.firstname;
+    var lastname = champs_json.lastname;
+    var email = champs_json.email;
+    var street = champs_json.street;
+    var city = champs_json.city;
+    var postalcode = champs_json.postalcode;
+    var phone = champs_json.phone;
+    var group = champs_json.group;
+
+    /**
+     * function de vérification with bind
+     * @param selector
+     * @param form_group_id
+     * @param opt
+     */
+    var liveVerification = function (selector, form_group_id, opt) {
+        opt = opt || {event:"keyup"};
+        selector.bind(opt.event, function () {
+            bootstrap_alert.reset(form_group_id);
+        });
+    };
+
+    var jsonFeedback = function (errors_tab, form_group_id) {
+        if(errors_tab != null)
+        {
+            bootstrap_alert.error(form_group_id, errors_tab);
+        }else{bootstrap_alert.reset(form_group_id);}
+    };
 
     // verifications en direct
-    firstname.bind("keyup", function () {
-        bootstrap_alert.reset(form_firstname);
-    });
-    lastname.bind("keyup", function () {
-        bootstrap_alert.reset(form_lastname);
-    });
-    email.bind("keyup", function () {
-        bootstrap_alert.reset(form_email);
-    });
-    street.bind("keyup", function () {
-        bootstrap_alert.reset(form_street);
-    });
-    city.bind("keyup", function () {
-        bootstrap_alert.reset(form_city);
-    });
-    postalcode.bind("keyup", function () {
-        bootstrap_alert.reset(form_postalcode);
-    });
-    phone.bind("keyup", function () {
-        bootstrap_alert.reset(form_phone);
-    });
-    group.on('change', function () {
-        bootstrap_alert.reset(form_group)
-    });
+    liveVerification(firstname,form_firstname);
+    liveVerification(lastname, form_lastname);
+    liveVerification(email, form_email);
+    liveVerification(street, form_street);
+    liveVerification(city, form_city);
+    liveVerification(postalcode, form_postalcode);
+    liveVerification(phone, form_phone, {event:"keyup"});
+    liveVerification(group, form_group, {event:"change"});
 
     bootstrap_alert();
     $('#add_user').on('submit', function (e) {
@@ -59,48 +78,26 @@ $(document).ready(function($){
                 //$form.find('#message').val('');
             })
             .done(function(data, textStatus, jqXHR){
-                //alert(data["success"]);
                 console.log(jQuery.parseJSON(jqXHR.responseText));
-                if(jqXHR.responseJSON['errors'] != undefined) {
-                    if(jqXHR.responseJSON['errors']['firstname'] != null)
-                    {
-                        bootstrap_alert.error(form_firstname, jqXHR.responseJSON['errors']['firstname']);
-                    }else{bootstrap_alert.reset(form_firstname);}
-                    if(jqXHR.responseJSON['errors']['lastname'] != null)
-                    {
-                        bootstrap_alert.error(form_lastname, jqXHR.responseJSON['errors']['lastname']);
-                    }else{bootstrap_alert.reset(form_lastname);}
-                    if(jqXHR.responseJSON['errors']['email'] != null)
-                    {
-                        bootstrap_alert.error(form_email, jqXHR.responseJSON['errors']['email']);
-                    }else{bootstrap_alert.reset(form_email);}
-                    if(jqXHR.responseJSON['errors']['street'] != null)
-                    {
-                        bootstrap_alert.error(form_street, jqXHR.responseJSON['errors']['street']);
-                    }else{bootstrap_alert.reset(form_street);}
-                    if(jqXHR.responseJSON['errors']['city'] != null)
-                    {
-                        bootstrap_alert.error(form_city, jqXHR.responseJSON['errors']['city']);
-                    }else{bootstrap_alert.reset(form_city);}
-                    if(jqXHR.responseJSON['errors']['postalCode'] != null)
-                    {
-                        bootstrap_alert.error(form_postalcode, jqXHR.responseJSON['errors']['postalCode']);
-                    }else{bootstrap_alert.reset(form_postalcode);}
-                    if(jqXHR.responseJSON['errors']['phone'] != null)
-                    {
-                        bootstrap_alert.error(form_phone, jqXHR.responseJSON['errors']['phone']);
-                    }else{bootstrap_alert.reset(form_phone);}
-                    if(jqXHR.responseJSON['errors']['group'] != null)
-                    {
-                        bootstrap_alert.error(form_group, jqXHR.responseJSON['errors']['group']);
-                    }
+                if(data['errors'] != undefined) {
+                    var errors = data['errors'];
+                    jsonFeedback(errors['firstname'], form_firstname);
+                    jsonFeedback(errors['lastname'], form_lastname);
+                    jsonFeedback(errors['email'], form_email);
+                    jsonFeedback(errors['street'], form_street);
+                    jsonFeedback(errors['city'], form_city);
+                    jsonFeedback(errors['postalCode'], form_postalcode);
+                    jsonFeedback(errors['phone'], form_phone);
+                    jsonFeedback(errors['group'], form_group);
 
-                    var errr = "";
-                    $.each(jqXHR.responseJSON['errors'], function(key, val){
-                        errr += val+'\n';
-                    });
-                    alert(errr);
-                } else {
+                    //var errr = "";
+                    //$.each(data['errors'], function (key, val) {
+                    //    errr += val + '\n';
+                    //});
+                    //alert(errr);
+                }
+                else
+                {
                     var type;
                     var title;
                     if(data["success"] != undefined){ type = 'success'; title = data["success"] }
